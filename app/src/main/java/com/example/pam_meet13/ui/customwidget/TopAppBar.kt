761955ccc28182d1cc55b16a2986d981864ee356
ulfhiraa <studyiraa@gmail.com>
2024-12-28 15:35:37 +0700

    ADD - fungsi CustomeTopAppBar untuk membuat top app bar kustom dengan dukungan navigasi kembali dan aksi refresh

---
+++ app/src/main/java/com/example/pam_meet13/ui/customwidget/TopAppBar.kt
@@ -0,0 +1,47 @@
+package com.example.pam_meet13.ui.customwidget
+
+import android.icu.text.CaseMap.Title
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+// untuk membuat top app bar kustom dengan dukungan navigasi kembali dan aksi refresh.
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomeTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: ()-> Unit = {},
+    onRefresh: ()-> Unit = {},
+){
+    CenterAlignedTopAppBar(
+        title = { Text(title)},
+        actions = {
+            Icon(imageVector = Icons.Default.Refresh,
+                contentDescription = "",
+                modifier = Modifier.clickable { onRefresh() }
+            )
+        },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior, navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = null
+                    )
+                }
+            }
+        }
+    )
+}
