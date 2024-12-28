77fb51153c3f9d9791a002283e0b4d73f8c43d50
ulfhiraa <studyiraa@gmail.com>
2024-12-28 15:07:00 +0700

    ADD - fungsi ekstensi Mahasiswa.toUiStateMhs() untuk mengonversi Mahasiswa menjadi InsertUiEvent

--- app/src/main/java/com/example/pam_meet13/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pam_meet13/ui/viewmodel/InsertViewModel.kt
@@ -58,6 +58,7 @@ fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
     angkatan = angkatan,
 )
 
+// fungsi ekstensi untuk mengonversi Mahasiswa menjadi InsertUiEvent
 fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
     insertUiEvent = toInsertUiEvent()
 )
