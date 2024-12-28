46fb2d1f5272cebb62e0135c5e2ed9fefe7510a3
ulfhiraa <studyiraa@gmail.com>
2024-12-28 14:25:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pam_meet13/repository/MahasiswaRepository.kt
@@ -0,0 +1,51 @@
+package com.example.pam_meet13.repository
+
+import com.example.pam_meet13.model.Mahasiswa
+import com.example.pam_meet13.service.MahasiswaService
+import java.io.IOException
+
+interface MahasiswaRepository {
+    suspend fun getMahasiswa(): List<Mahasiswa>
+
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
+
+    suspend fun deleteMahasiswa(nim: String )
+
+    suspend fun getMahasiswaByNim(nim: String):Mahasiswa
+}
+
+class NetworkMahasiswaRepository(
+    private val mahasiswaApiService: MahasiswaService
+) : MahasiswaRepository{
+
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete mahasiswa. HTTP Status Code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaApiService.getMahasiswa()
+
+    override suspend fun getMahasiswaByNim(nim: String): Mahasiswa {
+        return  mahasiswaApiService.getMahasiswaByNim(nim)
+    }
+}
---
+++ app/src/main/java/com/example/pam_meet13/service/MahasiswaService.kt
@@ -0,0 +1,34 @@
+package com.example.pam_meet13.service
+
+import com.example.pam_meet13.model.Mahasiswa
+//import okhttp3.Response
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface MahasiswaService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+
+    @GET("bacamahasiswa.php")
+    suspend fun getMahasiswa(): List<Mahasiswa>
+
+    @GET("baca1mahasiswa.php/{nim}")
+    suspend fun getMahasiswaByNim(@Query("nim") nim:String): Mahasiswa
+
+    @POST("insertmahasiswa.php")
+    suspend fun insertMahasiswa(@Body mahasiswa: Mahasiswa)
+
+    @PUT("editmahasiswa.php/{nim}")
+    suspend fun updateMahasiswa(@Query("nim") nim: String, @Body mahasiswa: Mahasiswa)
+
+    @DELETE("deletemahasiswa.php/{nim}")
+    suspend fun deleteMahasiswa(@Query("nim") nim:String):Response<Void>
+}
