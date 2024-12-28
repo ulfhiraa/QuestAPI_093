e879bcec6cd52e6b18666464a185025e22d9e4e8
ulfhiraa <studyiraa@gmail.com>
2024-12-28 14:47:13 +0700

    ADD - class MahasiswaContainer untuk mengimplementasikan AppContainer dengan konfigurasi Retrofit dan MahasiswaRepository.

--- app/src/main/java/com/example/pam_meet13/dependenciesinjection/MahasiswaContainer.kt
+++ app/src/main/java/com/example/pam_meet13/dependenciesinjection/MahasiswaContainer.kt
@@ -1,8 +1,32 @@
 package com.example.pam_meet13.dependenciesinjection
 
 import com.example.pam_meet13.repository.MahasiswaRepository
+import com.example.pam_meet13.repository.NetworkMahasiswaRepository
+import com.example.pam_meet13.service.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
 
 // interface AppContainer untuk menyediakan akses ke MahasiswaRepository.
 interface AppContainer{
     val mahasiswaRepository: MahasiswaRepository
+}
+
+// untuk mengimplementasikan AppContainer dengan konfigurasi Retrofit dan MahasiswaRepository.
+class MahasiswaContainer : AppContainer{
+
+    private val baseURL = "http://10.0.2.2:8000/umyTI" // localhost diganti ip kalo di run di hp
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseURL).build()
+
+    private val mahasiswaService: MahasiswaService by lazy {
+        retrofit.create(MahasiswaService::class.java)
+    }
+
+    override val mahasiswaRepository: MahasiswaRepository by lazy {
+        NetworkMahasiswaRepository(mahasiswaService)
+    }
 }
