cb4348d8af0d86074aed91423f4b7b7ba7b7ba7a
ulfhiraa <studyiraa@gmail.com>
2024-12-28 15:20:33 +0700

    ADD - interface DestinasiNavigasi.kt untuk mendefinisikan route dan titleRes dalam navigasi

---
+++ app/src/main/java/com/example/pam_meet13/ui/navigasi/DestinasiNavigasi.kt
@@ -0,0 +1,7 @@
+package com.example.pam_meet13.ui.navigasi
+
+// untuk mendefinisikan route dan titleRes dalam navigasi
+interface DestinasiNavigasi{
+    val route: String
+    val titleRes: String
+}
