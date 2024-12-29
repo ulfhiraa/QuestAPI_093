04d5696084ac97977e6513b39f84572babaf311f
ulfhiraa <studyiraa@gmail.com>
2024-12-29 12:13:39 +0700

    ADD - MahasiswaApp di MainActivity.kt untuk menjalankan program utama

--- app/src/main/java/com/example/pam_meet13/MainActivity.kt
+++ app/src/main/java/com/example/pam_meet13/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pam_meet13.ui.theme.PAM_Meet13Theme
+import com.example.pam_meet13.ui.viewmodel.MahasiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PAM_Meet13Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    MahasiswaApp()
             }
         }
     }
