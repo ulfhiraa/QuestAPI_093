2d749a71ebac59560ba42dea3d70090295513205
ulfhiraa <studyiraa@gmail.com>
2024-12-29 12:27:23 +0700

    ADD - MahasiswaContainer() pada MahasiswaApplications.kt

--- app/src/main/java/com/example/pam_meet13/MahasiswaApplications.kt
+++ app/src/main/java/com/example/pam_meet13/MahasiswaApplications.kt
@@ -2,12 +2,13 @@ package com.example.pam_meet13
 
 import android.app.Application
 import com.example.pam_meet13.dependenciesinjection.AppContainer
+import com.example.pam_meet13.dependenciesinjection.MahasiswaContainer
 
 //   untuk inisialisasi mengatur komponen yang akan dipakai di seluruh aplikasi
 class MahasiswaApplications:Application() {
     lateinit var container: AppContainer
     override fun onCreate() {
         super.onCreate()
-        container
+        container= MahasiswaContainer()
     }
 }
