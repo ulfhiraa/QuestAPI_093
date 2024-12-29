f31e5bd46d53efff13395076f7a56f543113a093
ulfhiraa <studyiraa@gmail.com>
2024-12-29 11:40:00 +0700

    ADD - fungsi deleteMhs untuk menghapus mahasiswa

--- app/src/main/java/com/example/pam_meet13/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pam_meet13/ui/viewmodel/HomeViewModel.kt
@@ -42,4 +42,16 @@ class HomeViewModel (private val mhs: MahasiswaRepository): ViewModel(){
             }
         }
     }
+
+    fun deleteMhs(nim: String){
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(nim)
+            } catch (e: IOException){
+                HomeUiState.Error
+            } catch (e:HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
 }
