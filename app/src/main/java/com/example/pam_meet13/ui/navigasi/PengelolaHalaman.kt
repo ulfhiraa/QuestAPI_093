48447b2f3a633a0796f17e781a16dfa78f659dda
ulfhiraa <studyiraa@gmail.com>
2024-12-30 09:58:37 +0700

    ADD - Composable Detail dan Update untuk navigasi layar pada PengelolaHalaman

--- app/src/main/java/com/example/pam_meet13/ui/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pam_meet13/ui/navigasi/PengelolaHalaman.kt
@@ -3,13 +3,19 @@ package com.example.pam_meet13.ui.navigasi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.pam_meet13.ui.view.DestinasiDetail
 import com.example.pam_meet13.ui.view.DestinasiEntry
 import com.example.pam_meet13.ui.view.DestinasiHome
+import com.example.pam_meet13.ui.view.DestinasiUpdate
+import com.example.pam_meet13.ui.view.DetailView
 import com.example.pam_meet13.ui.view.EntryMhsScreen
 import com.example.pam_meet13.ui.view.HomeScreen
+import com.example.pam_meet13.ui.view.UpdateView
 
 // fungsi PengelolaHalaman untuk mengatur navigasi antara layar Home
 // dan Entry Mahasiswa dengan rute dan transisi yang sesuai
@@ -22,15 +28,17 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
         startDestination = DestinasiHome.route,
         modifier = Modifier,
     ) {
+        // HOME
         composable(DestinasiHome.route){
             HomeScreen(
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route)},
-                onDetailClick = {
-
+                onDetailClick = { nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
                 }
             )
         }
 
+        // TAMBAH
         composable(DestinasiEntry.route){
             EntryMhsScreen(navigateBack = {
                 navController.navigate(DestinasiHome.route){
@@ -39,6 +47,54 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
                     }
                 }
             })
+        }
+
+        // DETAIL
+        composable(
+            DestinasiDetail.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiDetail.NIM) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val nim = it.arguments?.getString(DestinasiDetail.NIM)
+            nim?.let { nim ->
+                DetailView(
+                    navigateBack = {
+                        navController.popBackStack()
+                    },
+                    onEditClick = {
+                        navController.navigate("${DestinasiUpdate.route}/$nim")
+                    },
+                    nim = nim,
+                    onDeleteClick = {
+                        navController.popBackStack()
+                    }
+                )
+            }
+        }
+
+        // UPDATE
+        composable(
+            DestinasiUpdate.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiUpdate.NIM) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val nim = it.arguments?.getString(DestinasiUpdate.NIM)
+            nim?.let { nim ->
+                UpdateView(
+                    navigateBack = {
+                        navController.popBackStack()
+                    },
+                    onNavigateUp =  {
+                        navController.navigate(DestinasiHome.route) },
+                    nim = nim
+                )
+            }
         }
     }
 }
