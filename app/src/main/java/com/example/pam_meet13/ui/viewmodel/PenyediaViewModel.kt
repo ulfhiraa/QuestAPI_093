797ec3040bba8d3c4074095024ea3afb2dd8367f
ulfhiraa <studyiraa@gmail.com>
2024-12-30 09:54:59 +0700

    ADD - Inisialisasi Detail dan Update pada PenyediaViewModel

--- app/src/main/java/com/example/pam_meet13/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam_meet13/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.pam_meet13.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -9,8 +10,43 @@ import com.example.pam_meet13.MahasiswaApplications
 //  untuk membuat instance HomeViewModel dan InsertViewModel dengan dependensi MahasiswaRepository
 object PenyediaViewModel{
     val Factory = viewModelFactory {
-        initializer { HomeViewModel(MahasiswaApplications().container.mahasiswaRepository) }
-        initializer { InsertViewModel(MahasiswaApplications().container.mahasiswaRepository) }
+        // HOME
+        initializer {
+            HomeViewModel(
+                MahasiswaApplications()
+                    .container
+                    .mahasiswaRepository
+            )
+        }
+
+        // INSERT
+        initializer {
+            InsertViewModel(
+                MahasiswaApplications()
+                    .container
+                    .mahasiswaRepository
+            )
+        }
+
+        // DETAIL
+        initializer {
+            DetailViewModel(
+                createSavedStateHandle(),
+                MahasiswaApplications()
+                    .container
+                    .mahasiswaRepository
+            )
+        }
+
+        // UPDATE
+        initializer {
+            UpdateViewModel(
+                createSavedStateHandle(),
+                MahasiswaApplications()
+                    .container
+                    .mahasiswaRepository
+            )
+        }
     }
 
     fun CreationExtras.MahasiswaApplications(): MahasiswaApplications =
