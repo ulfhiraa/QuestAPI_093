084a95717b317060865e915cdf12f05cfe56f188
ulfhiraa <studyiraa@gmail.com>
2024-12-30 09:15:10 +0700

    UPDATE - MahasiswaRepository.kt

--- app/src/main/java/com/example/pam_meet13/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/pam_meet13/repository/MahasiswaRepository.kt
@@ -4,18 +4,23 @@ import com.example.pam_meet13.model.Mahasiswa
 import com.example.pam_meet13.service.MahasiswaService
 import java.io.IOException
 
+//repository network untuk operasi CRUD data mahasiswa
+// dengan integrasi API service
+
+// menampung operasi CRUD
 interface MahasiswaRepository {
     suspend fun getMahasiswa(): List<Mahasiswa>
+
+    suspend fun getMahasiswaByNim(nim: String):Mahasiswa //
 
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
 
     suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
 
     suspend fun deleteMahasiswa(nim: String )
-
-    suspend fun getMahasiswaById(nim: String):Mahasiswa
 }
 
+// untuk mengelola data mahasiswa
 class NetworkMahasiswaRepository(
     private val mahasiswaApiService: MahasiswaService
 ) : MahasiswaRepository{
@@ -45,7 +50,7 @@ class NetworkMahasiswaRepository(
 
     override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaApiService.getMahasiswa()
 
-    override suspend fun getMahasiswaById(nim: String): Mahasiswa {
+    override suspend fun getMahasiswaByNim(nim: String): Mahasiswa {
         return  mahasiswaApiService.getMahasiswaByNim(nim)
     }
 }
